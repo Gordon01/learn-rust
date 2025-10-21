75adab2f175e288bcb7739683f5dadb7455e285f
Alexander Sergeev <etc9053@gmail.com>
2025-10-22 00:51:28 +0300

    rayon: add clap

--- src/rayon/src/main.rs
+++ src/rayon/src/main.rs
@@ -1,21 +1,37 @@
 use std::time::{Duration, Instant};
 
+use clap::Parser;
 use rayon::prelude::*;
 
 use plot::plot_results;
 
 mod plot;
 
+#[derive(Parser)]
+struct Cli {
+    /// Size in megabytes.
+    #[arg(short, long, default_value_t = 10)]
+    size: u64,
+
+    /// A difference between chunk sizes.
+    #[arg(short, long, default_value_t = 10)]
+    step: usize,
+
+    /// Final vlue of chunk size.
+    #[arg(short, long, default_value_t = 1000)]
+    max_chunk: usize,
+}
+
 fn main() {
-    let size = 10 * 1024 * 1024;
+    let cli = Cli::parse();
+
+    let size = cli.size * 1024 * 1024;
     let data: Vec<_> = (0..size).collect();
-    let ts: Vec<_> = (1..=1000)
-        .step_by(10)
+    let ts: Vec<_> = (1..=cli.max_chunk)
+        .step_by(cli.step)
         .map(|c| (c, compare(&data, c)))
         .collect();
-    // for (chunk, (t1, t2)) in ts {
-    //     println!("chunk={chunk:4}: seq={t1:?}, par={t2:?}");
-    // }
+
     println!("{ts:?}");
     plot_results(&ts, "graph.png").expect("save output file");
 }
